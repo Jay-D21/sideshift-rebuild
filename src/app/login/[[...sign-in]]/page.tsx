b7da5a84ed842ad67994d625516197d4a43a9c5b
Jay-D21 <jaydeore.work@gmail.com>
2026-08-28 07:12:19 +0530

    fix: Clerk SSO callback 404 - move login to [[...sign-in]] catch-all and align env SIGN_IN_URL to /login

--- src/app/login/[[...sign-in]]/page.tsx
+++ src/app/login/[[...sign-in]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignIn } from '@clerk/nextjs'
 
-export default function LoginPage() {
+export default function LoginCatchAllPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50 px-4">
       <SignIn />
